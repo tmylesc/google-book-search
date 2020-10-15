9437cc3f4376ca43bff8766f41e1f78d4afc8a9a
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-15 17:18:49 -0400

    adding header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container">
+                <h1>(React) Google Books Search</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
