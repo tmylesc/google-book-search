9aa155501184f9c0ecd03783fda8c521d0275388
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-15 17:23:56 -0400

    adding nav component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container">
+                <Link className="navbar-brand" to="/">Google Books</Link>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/search">Search</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/saved">Saved</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
