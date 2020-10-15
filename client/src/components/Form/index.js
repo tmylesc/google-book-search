d8f29db11a064992aab6e81928586fa08efe8850
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-15 17:16:41 -0400

    adding form component

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Form(props) {
+    return (
+        <div className="container">
+            <form>
+                <div className="form-group">
+                    <label htmlFor="search">
+                        <h2>Search for and Save Books of Interest</h2>
+                    </label>
+                    <input
+                        onChange={props.handleInputChange}
+                        value={props.search}
+                        name="search"
+                        type="text"
+                        className="form-control"
+                        placeholder="Search for Books"
+                        id="search"
+                    />
+                    <button 
+                        onClick={props.handleFormSubmit}
+                        className="btn btn-primary mt-3 mb-3"
+                    >
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
