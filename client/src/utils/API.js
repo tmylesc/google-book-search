c91733e750724b5976b108e291e811ac7250214c
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-15 18:35:46 -0400

    adding api routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default {
+    getBook: function (query) {
+        return axios.get('https://www.googleapis.com/books/v1/volumes?q=${query}');
+    },
+
+    saveBook: function (bookData) {
+        return axios.post('/api/books', bookData)
+            .then(result => result.data);
+    },
+
+    deleteBook: function (id) {
+        return axios.delete('/api/books' + id)
+            .then(result => result.data);
+    },
+
+    savedBooks: function () {
+        return axios.get('/api/books')
+            .then(result => result.data);
+    }
+
+};
