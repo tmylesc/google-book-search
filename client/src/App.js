e7fc3278bb0fc99d05ebfe6e4b7b2ed57ea45f31
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-14 21:44:09 -0400

    adding skeleton for components/pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-}
+class App extends Component {
 
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Header />
+        <Wrapper>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Wrapper>
+      </Router>
+    )
+  }
+
+};
 
 export default App;
