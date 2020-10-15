37b0c0cbef37e798eae2f0e3a730199525b95ec6
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-15 18:25:00 -0400

    adding wrapper component

--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-export default function Wrapper() {
-    return (
-        <div>
-            Wrapper
-        </div>
-    )
-}
+function Wrapper(props) {
+    return <main className="wrapper" {...props} />;
+};
+
+export default Wrapper;
